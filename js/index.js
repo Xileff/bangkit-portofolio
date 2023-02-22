ba4a3607af77e8c8f575fd8f7c42881695145deb
Felix <felixsavero78@gmail.com>
2023-02-22 17:03:02 +0700

    navbar done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navItems = document.querySelectorAll('.nav-item');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('dropdown');
+    for (const navItem of navItems) {
+        navItem.classList.toggle('dropdown');
+    }
+});
